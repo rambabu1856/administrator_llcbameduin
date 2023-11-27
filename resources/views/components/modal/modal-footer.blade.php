03c52be144efa809cd446d99b5735eb0c6045a2a
rambabu1856 <ram.babullc@gmail.com>
2023-11-27 05:41:24 +0530

    first init

--- resources/views/components/modal/modal-footer.blade.php
+++ resources/views/components/modal/modal-footer.blade.php
@@ -1,8 +1,8 @@
 @props(['name'])
 
 <div class="modal-footer">
-  <button type="button" class="btn elevation-5 bg-pink" name="{{ $name }}" id="{{ $name }}"><i
+  <button type="button" class="btn elevation-1 bg-pink" name="{{ $name }}" id="{{ $name }}"><i
       class="fas fa-save mr-2"></i>Save</button>
-  <button type="button" class="btn elevation-5 bg-dark" data-dismiss="modal"><i
+  <button type="button" class="btn elevation-1 bg-dark" data-dismiss="modal"><i
       class="fa-solid fa-xmark mr-2"></i>Close</button>
 </div>
